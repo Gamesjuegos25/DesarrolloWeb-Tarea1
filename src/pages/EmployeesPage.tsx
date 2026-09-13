9070b40f5c4cfc2af652f587d16463704e3846c3
GamesJuegos25 <Gamesjuegos25@gmail.com>
2026-09-12 20:25:55 -0600

    version corregida de empleados funcional en render

--- src/pages/EmployeesPage.tsx
+++ src/pages/EmployeesPage.tsx
@@ -114,7 +114,7 @@ function EmployeesPage() {
         </div>
         <button
           onClick={handleOpenCreate}
-          className="px-4 py-2 bg-brand-800 hover:bg-brand-700 text-white rounded-lg text-sm font-medium transition-colors"
+          className="px-4 py-2 bg-brand-800 hover:bg-brand-700 text-black rounded-lg text-sm font-medium transition-colors"
         >
           + Nuevo empleado
         </button>
